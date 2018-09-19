f4a63329246fc3cd1fe78a44bd2650ec42704d6b
SVishC <chaitanyamvishnu@gmail.com>
2018-09-19 21:25:06 +0530

    ok

---
+++ MVCDemo/MVCDemo/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    [Table("tblDepartments")]
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
