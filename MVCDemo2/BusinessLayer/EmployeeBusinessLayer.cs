c7751cb4376ff92e028f7399016f582b66ef9709
SVishC <chaitanyamvishnu@gmail.com>
2018-09-19 21:12:45 +0530

    added 2nd project

---
+++ MVCDemo2/BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+        public IEnumerable<Employee> Employees
+        {
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                List<Employee> employees = new List<Employee>();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.Id = Convert.ToInt32(rdr["Id"]);
+                        employee.Name = Convert.ToString(rdr["Name"]);
+                        employee.Gender = Convert.ToString(rdr["Gender"]);
+                        employee.City = Convert.ToString(rdr["City"]);
+                        employee.DateOfBirth = Convert.ToDateTime(rdr["DateOfBirth"]);
+
+                        employees.Add(employee);
+                    }
+                }
+                    return employees;
+            }
+        }
+    }
+}
---
+++ MVCDemo2/MVCDemo2/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeList = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeList.Employees.ToList();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
